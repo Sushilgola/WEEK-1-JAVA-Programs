c0780015c2b891e68d5c207943491a4b362cc76f
Sushil Kumar <112944863+Sushilgola@users.noreply.github.com>
2026-02-27 20:15:03 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Address{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String: ");
+		String str1 = sc.nextLine();
+		System.out.println(str1);
+		
+}
+}
